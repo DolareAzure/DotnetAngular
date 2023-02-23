69f8fc914c75f957a92637d18df7fdcbebd9a9a9
suresh <sureshdolare03@gmail.com>
2023-02-23 20:35:17 +0530

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -7,12 +7,15 @@ builder.Services.AddDbContext<DataContext>(opt =>{
 opt.UseSqlite(builder.Configuration.GetConnectionString("Default"));
 });
 
+builder.Services.AddCors();
+
 // Add services to the container.
 
 builder.Services.AddControllers(); 
 
 var app = builder.Build();
 
+app.UseCors(corspolicy => corspolicy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
